99c4134a1ead147fd0d40d4a35d17fcd7c0ba9e5
Gye11 <hilallornekk@gmail.com>
2026-04-18 02:33:32 +0300

    final

--- src/db/initMongoConnection.js
+++ src/db/initMongoConnection.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 export const initMongoConnection = async () => {
   try {
-    await mongoose.connect(process.env.MONGODB_URL);
+    await mongoose.connect(process.env.MONGODB_URI);
 
     console.log("Mongo connection successfully established!");
   } catch (error) {
