97e69158c3abf2ba6d9e8e5b0196cb0edd150ea0
Gye11 <hilallornekk@gmail.com>
2026-05-03 01:53:56 +0300

    complete hw3 crud

---
+++ src/server.js
@@ -0,0 +1,15 @@
+import express from "express";
+import contactsRouter from "./routers/contacts.js";
+import notFoundHandler from "./middlewares/notFoundHandler.js";
+import errorHandler from "./middlewares/errorHandler.js";
+
+const app = express();
+
+app.use(express.json());
+
+app.use("/contacts", contactsRouter);
+
+app.use(notFoundHandler);
+app.use(errorHandler);
+
+export default app;
